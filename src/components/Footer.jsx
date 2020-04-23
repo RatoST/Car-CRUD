5922f8e400351d1aa1d49a4aba9ebc740311a534
Rato <47523773+RatoST@users.noreply.github.com>
2020-04-23 10:37:11 +0200

    addCar refactor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p>2020. © Ratko Pokrajac</p>
+      <p>{new Date().getFullYear()}. © Ratko Pokrajac</p>
     </footer>
   );
 }
