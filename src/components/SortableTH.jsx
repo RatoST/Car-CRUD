707d1672a728c54575f6c5cfe4f993a0e02b8e8e
Rato <47523773+RatoST@users.noreply.github.com>
2020-04-30 09:06:06 +0200

    add sortByName v4

--- src/components/SortableTH.jsx
+++ src/components/SortableTH.jsx
@@ -11,43 +11,36 @@ const SortableTH = ({ doSort, setThTitle /*doSortUns*/ }) => {
   const arrowUp = <>&#x2191;</>;
 
   const [thState, setThState] = useState(none);
-  const [iconState, setIconState] = useState(arrowUns);
-
-  console.log('initial state ' + thState);
+  const [iconState, setIconState] = useState(none);
 
   const iconBeh = () => {
     const getIcon = () => {
       if (thState === asc) {
         return arrowDown;
-        console.log('getIcon state ' + thState);
       } if (thState === desc) {
         return arrowUp;
       } return arrowUns;
     };
     setIconState(getIcon);
-    console.log('getIcon end state ' + thState);
   };
 
   const handleClick = () => {
     if (thState === none) {
       setThState(asc);
-      console.log('hanCL st ' + thState);      
     } if (thState === asc) {
       setThState(desc);
-      console.log('hanCl st2 ' + thState);
     } if (thState === desc) {
       setThState(asc);
-      console.log('hanCl st3 ' + thState);
     }
   };
 
   const doSortClick = () => {
-    if (thState === asc) {
+    if (thState === none) {
+      return null;
+    } if (thState === asc) {
       doSort(thTitle, thState);
-      console.log('doSort s1 ' + thState);
     } if (thState === desc) {
       doSort(thTitle, thState);
-      console.log('doSort s2 ' + thState);
     } return doSort(thTitle, thState);
   };
 
