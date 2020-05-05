3af2846833781cd876a3159bff717384fd759c45
Rato <47523773+RatoST@users.noreply.github.com>
2020-05-05 14:29:16 +0200

    validation functions

--- src/components/AddUpdateForm.jsx
+++ src/components/AddUpdateForm.jsx
@@ -2,65 +2,108 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-const AddUpdateForm = ({ car, handleInputChange }) => (
-  <>
-    <label>
-      Brand
-      <input
-        name="brand"
-        onChange={handleInputChange}
-        type="text"
-        value={car.brand}
-      />
-    </label>
-    <label>
-      Country
-      <input
-        name="country"
-        onChange={handleInputChange}
-        type="text"
-        value={car.country}
-      />
-    </label>
-    <label>
-      Model
-      <input
-        name="model"
-        onChange={handleInputChange}
-        type="text"
-        value={car.model}
-      />
-    </label>
-    <label>
-      Year
-      <input
-        name="year"
-        onChange={handleInputChange}
-        type="text"
-        value={car.year}
-      />
-    </label>
-    <label>
-      Serial Number
-      <input
-        name="serialNum"
-        onChange={handleInputChange}
-        type="text"
-        value={car.serialNum}
-      />
-    </label>
-    <label>
-      Description
-      <input
-        name="description"
-        onChange={handleInputChange}
-        type="text"
-        value={car.description}
-      />
-    </label>
-  </>
-);
+const AddUpdateForm = ({ car, handleInputChange }) => {
 
+  const N = 3;
+  const M = 10;
+
+  const validateEmpty = (inputText) => {
+    if (inputText > 1) {
+      return true;
+    } return 'Need to have at least 1 character.';
+  };
+
+  const validateNumber = (inputText) => {
+    if (typeof (inputText) === 'number') {
+      return true;
+    } return false;
+  };
+
+  const validateMinLength = (inputText) => {
+    const inputLength = inputText.length;
+    if (inputLength >= N) {
+      return true;
+    } return `Need to have at least ${N} characters.`;
+  };
+
+  const validateMaxLength = (inputText) => {
+    const inputLength = inputText.length;
+    if (inputLength <= M) {
+      return true;
+    } return `Max of ${M} characters allowed.`;
+  };
+
+  const validateMin = (inputText, inputNum) => {
+    if (typeof (inputText) === 'number' && inputText > inputNum) {
+      return true;
+    } return false;
+  };
+
+  const validateMax = (inputText, inputNum) => {
+    if (typeof (inputText) === 'number' && inputText < inputNum) {
+      return true;
+    } return false;
+  };
+
+  return (
+    <>
+      <label>
+        Brand
+        <input
+          name="brand"
+          onChange={handleInputChange}
+          type="text"
+          value={car.brand}
+        />
+      </label>
+      <label>
+        Country
+        <input
+          name="country"
+          onChange={handleInputChange}
+          type="text"
+          value={car.country}
+        />
+      </label>
+      <label>
+        Model
+        <input
+          name="model"
+          onChange={handleInputChange}
+          type="text"
+          value={car.model}
+        />
+      </label>
+      <label>
+        Year
+        <input
+          name="year"
+          onChange={handleInputChange}
+          type="text"
+          value={car.year}
+        />
+      </label>
+      <label>
+        Serial Number
+        <input
+          name="serialNum"
+          onChange={handleInputChange}
+          type="text"
+          value={car.serialNum}
+        />
+      </label>
+      <label>
+        Description
+        <input
+          name="description"
+          onChange={handleInputChange}
+          type="text"
+          value={car.description}
+        />
+      </label>
+    </>
+  );
+};
 AddUpdateForm.propTypes = {
   car: PropTypes.shape({
     id: PropTypes.string,
